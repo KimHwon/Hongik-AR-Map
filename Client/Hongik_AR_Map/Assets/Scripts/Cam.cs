baac91fce65c194ce48ec7cb317ecc8b3779cfeb
KimHwon <hwon233@gmail.com>
2021-11-29 18:00:49 +0900

    Fix merge conflict.

--- Client/Hongik_AR_Map/Assets/Scripts/Cam.cs
+++ Client/Hongik_AR_Map/Assets/Scripts/Cam.cs
@@ -37,67 +37,29 @@ public class Cam : MonoBehaviour
         webCamTexture.Play();
 
         StartCoroutine("TakePhoto");
-
-
-        // Require GPS permission.
-        UnityEngine.Android.Permission.RequestUserPermission("android.permission.ACCESS_FINE_LOCATION");
-        
-        Input.location.Start(5f, 5f);
     }
 
     // Update is called once per frame
     void Update()
     {
         for (int i = 0; i < buffer.Length; i++) buffer[i] = (byte)DataFormat.EMPTY;
-
         if (Input.touchCount > 0)
         {
-            FormatMessage("save");
+            int i;
+            byte[] msg = Encoding.UTF8.GetBytes("save");
+            buffer[0] = (byte)DataFormat.TEXT;
+            for (i = 0; i < Math.Min(buffer.Length-2, msg.Length); i++)
+                buffer[i+1] = msg[i];
+            buffer[i+1] = ETX;
         }
-        else if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
-        }
-        else if (onGPS)
-        {
-            float[] datas = new float[12];
-            for (int x = 0; x < datas.Length; x++) datas[x] = 0f;
-
-            datas[0] = Input.location.lastData.latitude;
-            datas[1] = Input.location.lastData.longitude;
-            datas[2] = Input.location.lastData.altitude;
-
-            FormatSensor(datas);
-
-            SetText("DebugText", "Location: " + datas[0].ToString() + ", " + datas[1].ToString());
-        }
-        else
-        {
-            
         }
         
         data_sock.Send(buffer, 1024, SocketFlags.None); // Client must send first.
         data_sock.Receive(buffer, 1024, SocketFlags.None);
         // do something with `buffer`.
-
-
-        if (!Input.location.isEnabledByUser)
-        {
-            SetText("DebugText", "Disabled");
-        }
-        else if (Input.location.status == LocationServiceStatus.Initializing)
-        {
-            SetText("DebugText", "Init");
-        }
-        else if (Input.location.status == LocationServiceStatus.Failed)
-        {
-            SetText("DebugText", "Failed");
-        }
-        else
-        {
-            onGPS = true;
-        }
-
     }
 
     void OnApplicationQuit()
@@ -115,14 +77,12 @@ public class Cam : MonoBehaviour
             data_sock.Send(buffer, 1024, SocketFlags.None);
             data_sock.Close();
         }
-
-
     }
 
     IEnumerator TakePhoto()
     {
         while (true) {
-            yield return new WaitForSeconds(.5f);
+            yield return new WaitForSeconds(.1f);
             // NOTE - you almost certainly have to do this here:
             yield return new WaitForEndOfFrame(); 
 
@@ -139,39 +99,13 @@ public class Cam : MonoBehaviour
                 //Encode to a JPG
                 byte[] bytes = photo.EncodeToJPG();
 
-                //SetText("DebugText", bytes.Length.ToString());
+                SetText("DebugText", bytes.Length.ToString());
 
                 image_sock.Send(bytes, SocketFlags.None);
             }
             catch (Exception e)
             {
                 SetText("DebugText", e.ToString());
-            }
-        }
-    }
-
-    void FormatMessage(string str)
-    {
-        int i;
-        byte[] msg = Encoding.UTF8.GetBytes(str);
-        buffer[0] = (byte)DataFormat.TEXT;
-        for (i = 0; i < Math.Min(buffer.Length-2, msg.Length); i++)
-            buffer[i+1] = msg[i];
-        buffer[i+1] = ETX;
-    }
-    void FormatSensor(float[] datas)
-    {
-        if (datas.Length != 12) return;
-
-        buffer[0] = (byte)DataFormat.SENSOR;
-        int i = 1;
-        for (int j = 0; j < 12; j++)
-        {
-            byte[] bytes = BitConverter.GetBytes(datas[j]);
-            for (int k = 0; k < bytes.Length; k++)
-            {
-                buffer[i] = bytes[k];
-                i++;
             }
         }
     }
